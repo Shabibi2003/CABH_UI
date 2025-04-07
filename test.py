facce25d912bbddd58cb61ffe9b53b24b117aeae
Yousuf Shabibi <145971672+Shabibi2003@users.noreply.github.com>
2025-04-07 16:34:08 +0530

    Update test.py

--- test.py
+++ test.py
@@ -298,8 +298,8 @@ if st.button("Generate Charts"):
                 plot_and_display_feature_heatmaps(indoor_df, features, year, selected_month)
                 
                 st.markdown("<br>", unsafe_allow_html= True)
-                st.markdown("<h3 style='font-size:24px;'>Line Charts of Indoor & Outdoor</h3>", unsafe_allow_html=True)
-                st.markdown("<br>")
+                st.markdown("<h3 style='font-size:30px; text-align:center; font-weight:bold';>Line Charts of Indoor & Outdoor</h3>", unsafe_allow_html=True)
+                st.markdown("<br>", unsafe_allow_html = True)
 
                 plot_and_display_line_charts(indoor_df, outdoor_df, pollutant_display_names)
 
